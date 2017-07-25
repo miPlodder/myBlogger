cf9efcab660e3340d7648fb59c9df2461b1a4d22
HanduSaksham <academic.rahul@gmail.com>
2017-07-25 23:28:16 +0530

    Http3 Firebase Offline Issue resolved

--- app/src/main/java/l8/cb/com/blogwithfirebase/BlogWithFirebase.java
+++ app/src/main/java/l8/cb/com/blogwithfirebase/BlogWithFirebase.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
 
@@ -19,7 +20,8 @@ public class BlogWithFirebase extends Application {
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         Picasso.Builder builder = new Picasso.Builder(this);
-        //builder.downloader(new OkHttpDownloader(this, Integer.MAX_VALUE));
+
+        builder.downloader(new OkHttp3Downloader(this, Integer.MAX_VALUE));
 
         Picasso built = builder.build();
         built.setIndicatorsEnabled(false); // to show the indicator
